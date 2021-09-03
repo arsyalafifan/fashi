46942e751ba482087560547e720e2dcadd22a034
Muh Arsyal Afifan Ersaputra <ersaputra21@gmail.com>
2021-09-03 14:04:03 +0700

    upload project vue

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderFashi/>
+    <HeroSection/>
+    <BannerFashi/>
+    <InstaFashi/>
+    <Logo/>
+    <FooterFashi/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderFashi from '@/components/HeaderFashi.vue';
+import HeroSection from '@/components/HeroSection.vue';
+import BannerFashi from '@/components/BannerFashi.vue';
+import InstaFashi from '@/components/InstaFashi.vue';
+import Logo from '@/components/Logo.vue';
+import FooterFashi from '@/components/FooterFashi.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderFashi,
+    HeroSection,
+    BannerFashi,
+    InstaFashi,
+    Logo,
+    FooterFashi
+  }
+}
+</script>
